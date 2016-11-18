84e0e3113d49937e5d9be8238c53a95c33fd4f2a
KoutarouYabe <idolm@ster.pw>
2016-11-19 04:24:10 +0900

    編集画面実装

--- assets/js/controller/UserStatusModalController.coffee
+++ assets/js/controller/UserStatusModalController.coffee
@@ -1,17 +1,23 @@
 angular.module "application"
 .controller "UserStatusModalController", ($rootScope, $scope, $http, $mdSidenav, $mdToast, $mdDialog, socketio, user, $timeout)->
+  $scope.showChangeInfo = false
   $scope.user = user
+
+  $scope.changeUser =
+    name: user.name
+    number: user.number
+
   now = new Date()
 
   $scope.heat =
     domain: "month"
     domainLabelFormat: '%Y-%m'
     data: "/attend/#{user.id}/heatmap"
-    start: new Date now.getFullYear(), now.getMonth()-11
+    start: new Date now.getFullYear(), now.getMonth() - 3
     cellSize: 20
-    range: 12
+    range: 4
     highlight: "now"
-    legend: [1,2,3,4]
+    legend: [1, 2, 3, 4]
 
   console.log user
 
@@ -20,6 +26,18 @@ angular.module "application"
 
   $scope.switchAttend = (user)->
     $http.post "/attend", user
+    .success (result)->
+      $mdToast.showSimple "Change attend status was successful!"
+      $scope.cancel()
+
+  $scope.switchChangeUserInfo = ($event)->
+    if $scope.showChangeInfo
+      if !$scope.changeUserValid.$valid
+        $mdToast.showSimple "名前と学籍番号を入力してください"
+        return
+      $http.put "/users/#{$scope.changeUser.number}", $scope.changeUser
       .success (result)->
-        $mdToast.showSimple "Change attend status was successful!"
+        $mdToast.showSimple "Change user info was successful!"
         $scope.cancel()
+    $scope.showChangeInfo = !$scope.showChangeInfo
+
--- www/js/controller/UserStatusModalController.js
+++ www/js/controller/UserStatusModalController.js
@@ -1,14 +1,19 @@
 angular.module("application").controller("UserStatusModalController", ["$rootScope", "$scope", "$http", "$mdSidenav", "$mdToast", "$mdDialog", "socketio", "user", "$timeout", function($rootScope, $scope, $http, $mdSidenav, $mdToast, $mdDialog, socketio, user, $timeout) {
   var now;
+  $scope.showChangeInfo = false;
   $scope.user = user;
+  $scope.changeUser = {
+    name: user.name,
+    number: user.number
+  };
   now = new Date();
   $scope.heat = {
     domain: "month",
     domainLabelFormat: '%Y-%m',
     data: "/attend/" + user.id + "/heatmap",
-    start: new Date(now.getFullYear(), now.getMonth() - 11),
+    start: new Date(now.getFullYear(), now.getMonth() - 3),
     cellSize: 20,
-    range: 12,
+    range: 4,
     highlight: "now",
     legend: [1, 2, 3, 4]
   };
@@ -16,10 +21,23 @@ angular.module("application").controller("UserStatusModalController", ["$rootSco
   $scope.cancel = function() {
     return $mdDialog.cancel();
   };
-  return $scope.switchAttend = function(user) {
+  $scope.switchAttend = function(user) {
     return $http.post("/attend", user).success(function(result) {
       $mdToast.showSimple("Change attend status was successful!");
       return $scope.cancel();
     });
+  };
+  return $scope.switchChangeUserInfo = function($event) {
+    if ($scope.showChangeInfo) {
+      if (!$scope.changeUserValid.$valid) {
+        $mdToast.showSimple("名前と学籍番号を入力してください");
+        return;
+      }
+      $http.put("/users/" + $scope.changeUser.number, $scope.changeUser).success(function(result) {
+        $mdToast.showSimple("Change user info was successful!");
+        return $scope.cancel();
+      });
+    }
+    return $scope.showChangeInfo = !$scope.showChangeInfo;
   };
 }]);
