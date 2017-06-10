72e852350b2267ab6dabd9f193300bc86db3f565
KoutarouYabe <idolm@ster.pw>
2017-06-10 23:33:00 +0900

    init v2

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { KokoiruPage } from './app.po';
+
+describe('kokoiru App', () => {
+  let page: KokoiruPage;
+
+  beforeEach(() => {
+    page = new KokoiruPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+  });
+});
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
